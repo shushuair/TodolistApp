82cc96ceace9e7c1a0568349abb4ab70db45d12f
shushuair <shushulkov1989@gmail.com>
2023-07-23 10:26:42 +0100

    add newtaskTitle, changeTaskStatus

--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,21 +1,21 @@
-// import {Checkbox, CheckboxPropsType} from "../components/Checkbox";
-// import {Meta, Story} from "@storybook/react";
-// import {action} from "@storybook/addon-actions"
-//
-// export default {
-//     title: "Todo/Checkbox",
-//     component: Checkbox,
-//      args: {
-//          callback: action("checkboxChanged"),
-//          statusChecked: false
-//      }
-// } as Meta
-//
-// const template:Story<CheckboxPropsType> = (args) => <Checkbox {...args} />
-//
-// export const checkboxChecked = template.bind({})
-// checkboxChecked.args = {statusChecked: true}
-//
-//
-// export const checkboxUnchecked = template.bind({})
-// checkboxUnchecked.args = {statusChecked: false}
+import {Checkbox, CheckboxPropsType} from "../components/Checkbox";
+import {Meta, Story} from "@storybook/react";
+import {action} from "@storybook/addon-actions"
+
+export default {
+    title: "Todo/Checkbox",
+    component: Checkbox,
+     args: {
+         callback: action("checkboxChanged"),
+         statusChecked: false
+     }
+} as Meta
+
+const template:Story<CheckboxPropsType> = (args) => <Checkbox {...args} />
+
+export const checkboxChecked = template.bind({})
+checkboxChecked.args = {statusChecked: true}
+
+
+export const checkboxUnchecked = template.bind({})
+checkboxUnchecked.args = {statusChecked: false}
